4e9a575d73141811f5c668139d640f18ee941791
Alexander Bocharov <alex.bocharov@outlook.com>
2020-07-29 08:25:49 +0300

    Add ConsoleApp sample

---
+++ samples/ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Dapper;
+using Microsoft.Extensions.DependencyInjection;
+using Parus.Data.Abstractions;
+using Parus.Data.Core;
+using Parus.Data.Oracle;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // register dependencies
+            var services = new ServiceCollection();
+            services.AddDataAccess(options => options.UseOracle("User Id=PARUS;Password=PASSWORD;Data Source=127.0.0.1:1521/ORCL"));
+            var serviceProvider = services.BuildServiceProvider();
+
+            // loading companies
+            using var connection = serviceProvider.GetRequiredService<IConnection>().ConnectionFactory.CreateConnection();
+            connection.Open();
+            
+            var result = connection.Query<string>("select name from companies");
+
+            Console.WriteLine("Companies:");
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
